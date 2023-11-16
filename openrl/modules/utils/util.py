adee5b984b0f3588e13a361433111b66c93c1966
Chen001117 <cwz19@mails.tsinghua.edu.cn>
2023-11-16 09:48:30 -0500

    update lr scheduler

--- openrl/modules/utils/util.py
+++ openrl/modules/utils/util.py
@@ -13,24 +13,13 @@ def get_grad_norm(it):
 def update_linear_schedule(optimizer, epoch, total_num_epochs, initial_lr):
     """Decreases the learning rate linearly"""
     # epoch = 3200 steps
-    lr = 1e-3
-    # if epoch < 900:
-    #     lr = 1e-3
-    # elif epoch < 1100: 
-    #     lr = (1e-3-1e-4) * ((1100-epoch)/200) + 1e-4
-    # elif epoch < 1900:
-    #     lr = 1e-4
-    # elif epoch < 2100: 
-    #     lr = (1e-4-1e-5) * ((2100-epoch)/200) + 1e-5
-    # elif epoch < 2900:
-    #     lr = 1e-5
-    # else:
-    #     lr = 1e-5 * ((total_num_epochs-epoch)/(total_num_epochs-2900))
-    # if epoch < 5:
-    #     lr = 1e-3 * (epoch+1) / 6
-    # else:
-    #     lr = 1e-5 + 0.5 * (1e-3-1e-5) * (1 + math.cos((epoch-5)/(total_num_epochs-5)*math.pi))
-    # lr = initial_lr - (initial_lr * (epoch / float(total_num_epochs)))
+    
+    # lr = 1e-3 * (epoch+1) / 6
+    # lr = (1e-4-1e-5) * ((2100-epoch)/200) + 1e-5
+    # lr = 1e-3 - (1e-3 * (epoch / float(total_num_epochs)))
+    # lr = 1e-4 + 0.5 * (1e-3-1e-4) * (1 + math.cos((epoch-5)/(3120)*math.pi))
+    
+    lr = initial_lr - (initial_lr * (epoch / float(total_num_epochs)))
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr
     
